906e997750d50568fbb41752038354b55ca7cd4a
Tahakbas <tahanumanakbas@gmail.com>
2022-03-06 15:19:03 +0300

    created config.py exampe

---
+++ example_config.py
@@ -0,0 +1,11 @@
+config = {
+    'PORT': 'your_port_number',
+    'HOST': 'your_host_address',
+    'DEBUG':  'true/false',
+    'AWS_SECRET_KEY': 'your_users_aws_secret_key_id',
+    'AWS_ACCESS_KEY': 'your_users_aws_access_key_id',
+    'AWS_REGION': 'your_users_region',
+    'LOG_FILE': 'your_log_file example app.log',
+    'LOG_LEVEL': 'log_level example INFO, ERROR',
+
+}
